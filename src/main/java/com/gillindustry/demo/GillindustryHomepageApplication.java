70824f07319f4e34c5d501eac27d6865ef46db55
chungchung234 <a12812@kyonggi.ac.kr>
2022-01-16 21:11:00 +0900

    .

---
+++ src/main/java/com/gillindustry/demo/GillindustryHomepageApplication.java
@@ -0,0 +1,13 @@
+package com.gillindustry.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GillindustryHomepageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GillindustryHomepageApplication.class, args);
+	}
+
+}
